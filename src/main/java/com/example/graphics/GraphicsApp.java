269e9285e6a65f09762788f47e471459b0f85eb0
Lara Cotti <lara.cotti@students.fhnw.ch>
2021-12-10 10:42:19 +0100

    Prüfung Übungen 1,2,3,4 und 5

--- src/main/java/com/example/graphics/GraphicsApp.java
+++ src/main/java/com/example/graphics/GraphicsApp.java
@@ -2,26 +2,26 @@ package com.example.graphics;
 
 import com.example.calculator.Calculator;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GraphicsApp extends Application {
 
     Calculator calc = new Calculator();
 
     @Override
-    public void start(Stage primaryStage) {
-        try {
-            BorderPane root = new BorderPane();
-            // root.setCenter(createInputPane());
-            Scene scene = new Scene(root,400,400);
+    public void start(Stage primaryStage) throws IOException {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("graphics.fxml"));
+            Scene scene = new Scene(loader.load(),600,400);
             primaryStage.setScene(scene);
             primaryStage.setTitle("Graphics App");
             primaryStage.show();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+
     }
     public static void main(String[] args) {
         launch(args);
